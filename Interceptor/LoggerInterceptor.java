01bc97fa8ad12d0df55483a567bf06f1fdcc5575
minhhoang98 <63001708+minhhoang98@users.noreply.github.com>
2020-04-01 20:58:02 +0700

    Add files via upload

---
+++ Interceptor/LoggerInterceptor.java
@@ -0,0 +1,28 @@
+package mushtaq.Interceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+
+   
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+ 
+		HttpSession session = request.getSession();
+
+		boolean isLogined = session.getAttribute("check") != null ? (Boolean) session.getAttribute("check") : false;
+
+		if (!isLogined) {
+			response.sendRedirect("login");
+			return false;
+
+		}
+
+		return true;
+
+	}
+
+	
+}
